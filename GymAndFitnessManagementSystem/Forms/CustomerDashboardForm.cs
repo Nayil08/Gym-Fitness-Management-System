90258bdca25a644b7709b99afec0aa174b44e92a
Sadia Arifin Shimu <sadiaarifinshimu18@gmail.com>
2026-09-11 15:21:23 +0600

    Improve customer dashboard UI and customer module

--- GymAndFitnessManagementSystem/Forms/CustomerDashboardForm.cs
+++ GymAndFitnessManagementSystem/Forms/CustomerDashboardForm.cs
@@ -9,7 +9,7 @@ namespace GymAndFitnessManagementSystem.Forms
     {
         public CustomerDashboardForm()
         {
-            Theme.Apply(this, "Customer Dashboard");
+            startposition = FormstartPosition.centerScreen; Theme.Apply(this, "Customer Dashboard");
 
             // MAIN LAYOUT
             var root = new TableLayoutPanel();
